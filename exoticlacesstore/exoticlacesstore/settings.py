8b477bad18e7457733d2eaf35070cb945d9f890b
abualameen <abulyaqs@gmail.com>
2026-06-23 14:54:37 -0700

    Switch to DigitalOcean Managed Database

--- exoticlacesstore/exoticlacesstore/settings.py
+++ exoticlacesstore/exoticlacesstore/settings.py
@@ -109,17 +109,24 @@ WSGI_APPLICATION = 'exoticlacesstore.wsgi.application'
 
 
 
+# settings.py
+import os
+from decouple import config
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': config('DB_NAME', default='exoticlaces_db'),
-        'USER': config('DB_USER', default='exoticlaces_user'),
-        'PASSWORD': config('DB_PASSWORD', default=''),
-        'HOST': config('DB_HOST', default='localhost'),
-        'PORT': config('DB_PORT', default='3306'),  # Keep as string, will be converted
+        'NAME': config('DB_NAME', default='defaultdb'),
+        'USER': config('DB_USER', default='doadmin'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT', default='25060'),
         'OPTIONS': {
             'charset': 'utf8mb4',
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'ssl': {
+                'ca': os.path.join(BASE_DIR, 'ca-certificate.crt')
+            },
         },
     }
 }
