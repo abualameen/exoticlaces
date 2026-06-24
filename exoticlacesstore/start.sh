2a8e43c3e9ec43d4fbf8e29bc5cad9a8b5dc1cf2
abualameen <abulyaqs@gmail.com>
2026-06-24 11:12:10 -0700

    reverting .sh file

--- exoticlacesstore/start.sh
+++ exoticlacesstore/start.sh
@@ -28,16 +28,11 @@ else:
 EOF
 
 # Start Gunicorn
-# Start Gunicorn with more robust settings
 echo "Starting Gunicorn..."
 exec gunicorn --bind 0.0.0.0:8000 \
     --workers 2 \
-    --threads 4 \  # Increase threads
+    --threads 2 \
     --worker-tmp-dir /dev/shm \
-    --timeout 120 \  # Increase timeout
-    --graceful-timeout 30 \
-    --keep-alive 5 \
-    --max-requests 1000 \
-    --max-requests-jitter 100 \
+    --timeout 120 \
     --log-level info \
     exoticlacesstore.wsgi:application
