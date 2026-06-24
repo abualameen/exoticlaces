e20eee3dea558092cc0484751b9a87c4a68c55a8
abualameen <abulyaqs@gmail.com>
2026-06-24 01:59:30 -0700

    Update Dockerfile with certificate and fix settings.py path

--- exoticlacesstore/exoticlacesstore/settings.py
+++ exoticlacesstore/exoticlacesstore/settings.py
@@ -124,9 +124,9 @@ DATABASES = {
         'OPTIONS': {
             'charset': 'utf8mb4',
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-            'ssl': {
-                'ca': os.path.join(BASE_DIR, 'ca-certificate.crt')
-            },
+            # 'ssl': {
+            #     'ca': os.path.join(BASE_DIR, 'ca-certificate.crt')
+            # },
         },
     }
 }
