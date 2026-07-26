4a8f53a453d7fdc401b248c8764ec2d4883fef27
abualameen <abulyaqs@gmail.com>
2026-07-26 21:10:14 +0100

    sign in real users only1

--- exoticlacesstore/lacesstore/apps.py
+++ exoticlacesstore/lacesstore/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class LacesstoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'lacesstore'
+
+
+    def ready(self):
+        import lacesstore.signals  # Register the signals
