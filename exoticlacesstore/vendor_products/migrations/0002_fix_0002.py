54e67d21559f4a7fea13532079ea48c4113eabce
abualameen <abulyaqs@gmail.com>
2026-07-30 17:31:49 +0100

    Replace problematic migration with empty migration

---
+++ exoticlacesstore/vendor_products/migrations/0002_fix_0002.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('vendor_products', '0001_initial'),
+    ]
+
+    operations = [
+        # This migration intentionally does nothing
+        # The tables already exist, and the fields were added manually
+    ]
